39c5efe22cfd4bb92ec5d3df000278374e3e92a1
valentimkoniarski <66980970+valentimkoniarski@users.noreply.github.com>
2023-06-24 22:50:56 -0300

    creation of ProductService unit tests

--- src/main/java/crudproducts/crudproductsbackend/services/ProductService.java
+++ src/main/java/crudproducts/crudproductsbackend/services/ProductService.java
@@ -1,9 +1,9 @@
 package crudproducts.crudproductsbackend.services;
 
 import crudproducts.crudproductsbackend.dto.CategoryDto;
-import crudproducts.crudproductsbackend.dto.product.ProductDto;
 import crudproducts.crudproductsbackend.dto.ProductImage.ProductImageDto;
 import crudproducts.crudproductsbackend.dto.UserDto;
+import crudproducts.crudproductsbackend.dto.product.ProductDto;
 import crudproducts.crudproductsbackend.dto.product.ProductSimplifiedDto;
 import crudproducts.crudproductsbackend.entities.Category;
 import crudproducts.crudproductsbackend.entities.Product;
@@ -16,55 +16,54 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+
+import static java.util.Objects.isNull;
 
 @Service
 @AllArgsConstructor
 @Transactional
 public class ProductService {
 
-    private final ProductRepository repository;
+    private final ProductRepository productRepository;
     private final UserService userService;
     private final ModelMapper modelMapper;
     private final ProductImageRepository productImageRepository;
+    private final static String EMPTY_IMAGE_PRODUCT = "https://ingoodcompany.asia/images/products_attr_img/matrix/default.png";
 
     public List<ProductDto> findAll(final HttpServletRequest request) {
-        Long idUserByToken = userService.getIdUserByToken(request);
-        Iterable<Product> allByUserId = repository.findAllByUserId(idUserByToken);
-        List<ProductDto> productDtoList = getProductSimplifiedDto(allByUserId);
-        return productDtoList;
+        final Long idUserByToken = userService.getIdUserByToken(request);
+        final Iterable<Product> allByUserId = productRepository.findAllByUserId(idUserByToken);
+        return getProductSimplifiedDto(allByUserId);
     }
 
     public List<ProductDto> findAllDeleted(final HttpServletRequest request) {
-        Long idUserByToken = userService.getIdUserByToken(request);
-        Iterable<Product> allByUserId = repository.findAllByUserIdDeleted(idUserByToken);
-        List<ProductDto> productDtoList = getProductSimplifiedDto(allByUserId);
-        return productDtoList;
+        final Long idUserByToken = userService.getIdUserByToken(request);
+        final Iterable<Product> allByUserId = productRepository.findAllByUserIdDeleted(idUserByToken);
+        return getProductSimplifiedDto(allByUserId);
     }
 
     public ProductSimplifiedDto findById(final Long productId, final HttpServletRequest request) {
-        Long idUserByToken = userService.getIdUserByToken(request);
-        Product product = repository.findByIdAndUserId(productId, idUserByToken);
+        final Long idUserByToken = userService.getIdUserByToken(request);
+        final Product product = productRepository.findByIdAndUserId(productId, idUserByToken);
         return getProductSimplifiedDto(product);
     }
 
     public ProductSimplifiedDto save(final ProductSimplifiedDto productSimplifiedDto, final HttpServletRequest request) {
-        productSimplifiedDto.setCategoryDto(productSimplifiedDto.getCategoryDto());
-        Product product = modelMapper.map(productSimplifiedDto, Product.class);
+        final Product product = modelMapper.map(productSimplifiedDto, Product.class);
         product.setUser(userService.getUserByToken(request));
-        repository.save(product);
+        productRepository.save(product);
         return getProductSimplifiedDto(product);
     }
 
     public ProductSimplifiedDto update(final Long productId, final ProductDto productDto, final HttpServletRequest request) {
-        Long idUserByToken = userService.getIdUserByToken(request);
-        Product product = repository.findByIdAndUserId(productId, idUserByToken);
+        final Long idUserByToken = userService.getIdUserByToken(request);
+        final Product product = productRepository.findByIdAndUserId(productId, idUserByToken);
         setProduct(productDto, request, product);
-        repository.save(product);
+        productRepository.save(product);
         return getProductSimplifiedDto(product);
     }
 
@@ -79,35 +78,13 @@ public class ProductService {
         product.setCategory(category);
     }
 
-    public ProductSimplifiedDto delete(Long id, HttpServletRequest request) {
-        Long idUserByToken = userService.getIdUserByToken(request);
-        repository.deleteProductByIdAndUserId(id, idUserByToken);
+    public ProductSimplifiedDto delete(final Long id, final HttpServletRequest request) {
+        final Long idUserByToken = userService.getIdUserByToken(request);
+        productRepository.deleteProductByIdAndUserId(id, idUserByToken);
 
-        Product product = repository.getOne(id);
+        final Product product = productRepository.getOne(id);
         product.setDeleted(true);
         return getProductSimplifiedDto(product);
-
-    }
-
-    public List<ProductDto> getProductSimplifiedDto(final Iterable<Product> allByUserId) {
-        return StreamSupport.stream(allByUserId.spliterator(), false)
-                .map(product -> {
-                    final UserDto userDto = modelMapper.map(product.getUser(), UserDto.class);
-                    final ProductDto productDto = modelMapper.map(product, ProductDto.class);
-
-                    productDto.setUserDto(userDto);
-
-                    final ProductImage byPrincipalTrueAndProductId = productImageRepository.findByPrincipalTrueAndProductId(product.getId());
-
-                    if (byPrincipalTrueAndProductId != null) {
-                        final ProductImageDto productImageDto = modelMapper.map(byPrincipalTrueAndProductId, ProductImageDto.class);
-                        productDto.setProductImageDto(Collections.singletonList(productImageDto));
-                    }
-
-
-                    return productDto;
-                })
-                .collect(Collectors.toList());
     }
 
     private ProductSimplifiedDto getProductSimplifiedDto(final Product product) {
@@ -117,23 +94,28 @@ public class ProductService {
         return productSimplifiedDto;
     }
 
-    private List<ProductImageDto> getProductImageDto(final Product product) {
-        final List<ProductImageDto> listProductImagesDto = new ArrayList<>();
-
-        product.getProductImages().forEach(productImage -> {
-            final ProductImageDto productImageDto = modelMapper.map(product.getProductImages(), ProductImageDto.class);
-
-            productImageDto.setId(productImage.getId());
-            productImageDto.setName(productImage.getName());
-            productImageDto.setUrl(productImage.getUrl());
-            productImageDto.setType(productImage.getType());
-            productImageDto.setPrincipal(productImage.getPrincipal());
-
-
-            listProductImagesDto.add(productImageDto);
-        });
-        return listProductImagesDto;
+    public List<ProductDto> getProductSimplifiedDto(final Iterable<Product> allProductsByUserId) {
+        return StreamSupport.stream(allProductsByUserId.spliterator(), false)
+                .map(this::mapProduct)
+                .collect(Collectors.toList());
     }
 
+    private ProductDto mapProduct(final Product product) {
+        final UserDto userDto = modelMapper.map(product.getUser(), UserDto.class);
+        final ProductDto productDto = modelMapper.map(product, ProductDto.class);
+        productDto.setUserDto(userDto);
 
+        final ProductImage principalTrueByProductId = productImageRepository.findPrincipalTrueByProductId(product.getId());
+        if (isNull(principalTrueByProductId)) {
+            final ProductImageDto productImageDto = new ProductImageDto();
+            productImageDto.setPrincipal(false);
+            productImageDto.setUrl(EMPTY_IMAGE_PRODUCT);
+            productDto.setProductImageDto(Collections.singletonList(productImageDto));
+        } else {
+            final ProductImageDto productImageDto = modelMapper.map(principalTrueByProductId, ProductImageDto.class);
+            productDto.setProductImageDto(Collections.singletonList(productImageDto));
+        }
+
+        return productDto;
+    }
 }
---
+++ src/test/java/crudproducts/crudproductsbackend/services/ProductServiceTest.java
@@ -0,0 +1,194 @@
+package crudproducts.crudproductsbackend.services;
+
+import crudproducts.crudproductsbackend.dto.CategoryDto;
+import crudproducts.crudproductsbackend.dto.UserDto;
+import crudproducts.crudproductsbackend.dto.product.ProductDto;
+import crudproducts.crudproductsbackend.dto.product.ProductSimplifiedDto;
+import crudproducts.crudproductsbackend.entities.Category;
+import crudproducts.crudproductsbackend.entities.Product;
+import crudproducts.crudproductsbackend.entities.ProductImage;
+import crudproducts.crudproductsbackend.entities.User;
+import crudproducts.crudproductsbackend.repositories.ProductImageRepository;
+import crudproducts.crudproductsbackend.repositories.ProductRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private UserService userService;
+
+    @Spy
+    ModelMapper modelMapper = new ModelMapper();
+
+    @Mock
+    private ProductImageRepository productImageRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Mock
+    private HttpServletRequest request;
+
+    private final static Long USER_ID = 1L;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(userService.getIdUserByToken(request)).thenReturn(1L);
+        productService = new ProductService(productRepository, userService, modelMapper, productImageRepository);
+    }
+
+    @BeforeEach
+    public void setUpMock() {
+        final Product product = createProduct();
+
+        doAnswer(invocation -> {
+            final User user = invocation.getArgument(0);
+            final UserDto userDto = new UserDto();
+            userDto.setEmail(user.getEmail());
+
+            final ProductDto productDto = new ProductDto();
+            productDto.setUserDto(userDto);
+            return productDto;
+        }).when(modelMapper).map(product.getUser(), any(User.class));
+
+        doAnswer(invocation -> {
+            final ProductDto createProductDto = invocation.getArgument(0);
+            final ProductDto productDto = new ProductDto();
+            productDto.setId(createProductDto.getId());
+            productDto.setName(createProductDto.getName());
+            productDto.setPrice(createProductDto.getPrice());
+            return productDto;
+        }).when(modelMapper).map(product, any(ProductDto.class));
+
+        doAnswer(invocation -> {
+            final Category category = invocation.getArgument(0);
+            final CategoryDto categoryDto = new CategoryDto();
+            categoryDto.setId(category.getId());
+            categoryDto.setName(category.getName());
+            return categoryDto;
+        }).when(modelMapper).map(product.getCategory(), any(CategoryDto.class));
+    }
+
+    @Test
+    public void testFindAllWithPrincipalImage() {
+        final Product product = createProduct();
+        final List<Product> products = Collections.singletonList(product);
+        when(userService.getIdUserByToken(request)).thenReturn(USER_ID);
+        when(productRepository.findAllByUserId(USER_ID)).thenReturn(products);
+        when(productImageRepository.findPrincipalTrueByProductId(anyLong())).thenReturn(createProductImageWithPrincipal());
+        productService.findAll(request);
+        verify(productRepository, times(1)).findAllByUserId(USER_ID);
+    }
+
+    @Test
+    public void testFindAllWithoutPrincipalImage() {
+        final Product product = createProduct();
+        final List<Product> products = Collections.singletonList(product);
+        when(userService.getIdUserByToken(request)).thenReturn(USER_ID);
+        when(productRepository.findAllByUserId(USER_ID)).thenReturn(products);
+        when(productImageRepository.findPrincipalTrueByProductId(anyLong())).thenReturn(null);
+        productService.findAll(request);
+        verify(productRepository, times(1)).findAllByUserId(USER_ID);
+    }
+
+    @Test
+    public void testFindAllDeleted() {
+        when(userService.getIdUserByToken(request)).thenReturn(USER_ID);
+        when(productRepository.findAllByUserIdDeleted(USER_ID)).thenReturn(Collections.emptyList());
+        productService.findAllDeleted(request);
+        verify(productRepository, times(1)).findAllByUserIdDeleted(USER_ID);
+    }
+
+    @Test
+    public void testFindById() {
+        when(userService.getIdUserByToken(request)).thenReturn(USER_ID);
+        when(productRepository.findByIdAndUserId(anyLong(), anyLong())).thenReturn(createProduct());
+        productService.findById(1L, request);
+        verify(productRepository, times(1)).findByIdAndUserId(anyLong(), anyLong());
+    }
+
+    @Test
+    public void testSave() {
+        final ProductSimplifiedDto productSimplifiedDto = new ProductSimplifiedDto();
+        productSimplifiedDto.setName("name");
+        productSimplifiedDto.setDescription("description");
+        final CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setId(1L);
+        categoryDto.setName("name");
+        productSimplifiedDto.setCategoryDto(categoryDto);
+
+        productService.save(productSimplifiedDto, request);
+
+        final Product product = modelMapper.map(productSimplifiedDto, Product.class);
+        verify(productRepository, times(1)).save(product);
+    }
+
+    @Test
+    public void testUpdate() {
+        when(userService.getIdUserByToken(request)).thenReturn(USER_ID);
+        when(productRepository.findByIdAndUserId(anyLong(), anyLong())).thenReturn(createProduct());
+        when(userService.getUserByToken(request)).thenReturn(new User());
+
+        productService.update(1L, createProductDto(), request);
+        verify(productRepository, times(1)).save(createProduct());
+    }
+
+    @Test
+    public void testDelete() {
+        when(userService.getIdUserByToken(request)).thenReturn(USER_ID);
+        when(productRepository.getOne(1L)).thenReturn(createProduct());
+        productService.delete(1L, request);
+        verify(productRepository, times(1)).deleteProductByIdAndUserId(anyLong(), anyLong());
+    }
+
+    public Product createProduct() {
+        final Product product = new Product();
+        product.setId(1L);
+        product.setName("name");
+        product.setDescription("description");
+        product.setUser(new User());
+        product.setCategory(new Category());
+        return product;
+    }
+
+    public ProductDto createProductDto() {
+        final ProductDto productDto = new ProductDto();
+        productDto.setId(1L);
+        productDto.setName("name");
+        productDto.setDescription("description");
+        productDto.setUserDto(new UserDto());
+        productDto.setCategoryDto(new CategoryDto());
+        return productDto;
+    }
+
+    public ProductImage createProductImageWithPrincipal() {
+        final ProductImage productImage = new ProductImage();
+        productImage.setId(1L);
+        productImage.setPrincipal(true);
+        productImage.setUrl("url");
+        productImage.setProduct(new Product());
+        return productImage;
+    }
+}
