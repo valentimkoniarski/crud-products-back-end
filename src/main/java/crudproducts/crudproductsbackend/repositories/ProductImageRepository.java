be2bd7f6c731cc69448507b9df67756209a5fdc0
valentimkoniarski <66980970+valentimkoniarski@users.noreply.github.com>
2023-06-24 22:50:15 -0300

    adjusted for more suitable name

--- src/main/java/crudproducts/crudproductsbackend/repositories/ProductImageRepository.java
+++ src/main/java/crudproducts/crudproductsbackend/repositories/ProductImageRepository.java
@@ -12,23 +12,23 @@ import java.util.List;
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 
     @Query("SELECT pi FROM ProductImage pi WHERE pi.id = :idProductImage AND pi.user.id = :userId")
-    ProductImage findByIdAndUserId(@Param("idProductImage") Long idProductImage, @Param("userId") Long userId);
+    ProductImage findByIdAndUserId(@Param("idProductImage") final Long idProductImage, @Param("userId") final Long userId);
 
     @Query("SELECT pi FROM ProductImage pi WHERE pi.product.id = :productId AND pi.user.id = :userId")
-    List<ProductImage> findAllByProductIdAndUserId(@Param("productId") Long productId, @Param("userId") Long userId);
+    List<ProductImage> findAllByProductIdAndUserId(@Param("productId") final Long productId, @Param("userId") final Long userId);
 
     @Query("SELECT pi FROM ProductImage pi WHERE pi.principal = true AND pi.product.id = ?1")
-    ProductImage findByPrincipalTrueAndProductId(Long productId);
+    ProductImage findPrincipalTrueByProductId(final Long productId);
 
     @Transactional
     @Modifying(flushAutomatically = true)
     @Query("UPDATE ProductImage pi SET pi.principal = CASE WHEN pi.id = :id THEN true ELSE false END WHERE pi.product.id = :productId AND pi.user.id = :userId")
-    void setPrincipal(Long productId, Long id, Long userId);
+    void setPrincipal(final Long productId, Long id, final Long userId);
 
     @Transactional
     @Modifying
     @Query("DELETE FROM ProductImage pi WHERE pi.id = :idProductImage AND pi.user.id = :userId")
-    void deleteByIdAndUserId(@Param("idProductImage") Long idProductImage, @Param("userId") Long userId);
+    void deleteByIdAndUserId(@Param("idProductImage") final Long idProductImage, @Param("userId") final  Long userId);
 
 
 }
