e0d78e995b871620cce347751c40748756f4e523
valentimkoniarski <66980970+valentimkoniarski@users.noreply.github.com>
2023-07-10 18:49:26 -0300

    removido entidade e afins de ProductImage

--- src/main/java/crudproducts/crudproductsbackend/entities/Product.java
+++ src/main/java/crudproducts/crudproductsbackend/entities/Product.java
@@ -1,45 +1,47 @@
 package crudproducts.crudproductsbackend.entities;
 
 import crudproducts.crudproductsbackend.listener.HistoryListener;
-import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
-import java.util.List;
 
 @Entity
 @Table(name = "tb_products")
-@Data
 @Getter
+@Setter
 @EntityListeners(HistoryListener.class)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", referencedColumnName = "id")
-    private List<ProductImage> productImages;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @NotNull(message = "User cannot be null")
     private User user;
 
     @JoinColumn(name = "category_id", referencedColumnName = "id")
     @OneToOne(fetch = FetchType.LAZY)
+    @NotNull(message = "Category cannot be null")
     private Category category;
 
+    @Size(max = 15, min = 5)
+    @NotNull(message = "Name cannot be null")
     private String name;
 
+    @Size(max = 80, min = 5)
+    @NotNull(message = "Description cannot be null")
     private String description;
 
+    @NotNull(message = "Price cannot be null")
     private BigDecimal price;
 
     private String createdBy;
 
     @Column(name = "is_deleted")
     private Boolean deleted = false;
-
-
 }
