c0b1bd3566e7ccf4a392183c0f066a3cc3ee60d8
valentimkoniarski <66980970+valentimkoniarski@users.noreply.github.com>
2023-07-10 18:48:24 -0300

    removido @Data

--- src/main/java/crudproducts/crudproductsbackend/entities/Category.java
+++ src/main/java/crudproducts/crudproductsbackend/entities/Category.java
@@ -1,14 +1,16 @@
 package crudproducts.crudproductsbackend.entities;
 
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tb_categories")
-@Data
+@Getter
+@Setter
 public class Category {
 
     @Id
